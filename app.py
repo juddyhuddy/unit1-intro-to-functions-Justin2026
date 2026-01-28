7e35850a42faf6ff63390fa073cf6f5365b33ef0
juddyhuddy <justinl732@nycstudents.net>
2026-01-28 09:25:06 -0500

    jutin

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+
+t.forward(200)
+t.forward(100)
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(90)
+
+turtle.done()
