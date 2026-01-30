4e67b050de7331ee9deb81c364afe868c48bd535
juddyhuddy <justinl732@nycstudents.net>
2026-01-30 08:48:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,17 @@ from turtle import *
 t = Turtle()
 t.shape('turtle')
 
-def square(x):
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-square(200)
-def equal(x):
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-equal(200) 
+def square(x, y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
 
-def right():
-    t.backward(100)
-    t.right(90)
-    t.forward(100)
-    t.left(135)
-    t.forward(142)
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+doubleSquares(5)
+
 
 turtle.done()
